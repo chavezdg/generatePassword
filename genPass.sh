fa6100d3867938503b3e121c348fd7a43921989a
davidc <david.geo.chavez@gmail.com>
2019-08-12 08:23:36 -0700

    This is a password generator for Linux.

---
+++ genPass.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# This is a random password generator.
+# Created by davidc
+# Version 1
+
+# Variable to get first argument for length of password.
+randomLine=$1
+
+# Function to run the random generator.
+function randomF(){
+< /dev/urandom tr -dc a-zA-Z0-9 | head -c$randomLine; echo
+}
+
+# Conditions for negative numbers, letters, and numbers.
+if [[ $# -eq 0 ]]; then
+  randomLine=$( shuf -i 8-32 -n 1 )
+  randomF
+ elif [[ "$1" -eq "" ]] &>/dev/null; then
+  randomLine=${#1}
+  randomF
+ elif [[ "$1" -lt 0 ]] &>/dev/null; then
+  randomLine=$( echo $1 | cut --complement -c 1 )
+  randomF
+ elif [[ "$1" -gt 0 ]] &>/dev/null; then
+  randomF
+ else
+  echo "ARGUMENT MUST BE NUMBERS OR LETTERS"
+fi
+
